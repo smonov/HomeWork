6b97ea9825a38d4b5ec0470f67a07c5bce371d81
smonov <smonov@abv.bg>
2015-09-30 18:57:29 +0300

    Week8

---
+++ Week8/sets.py
@@ -0,0 +1,58 @@
+#Week8 task1 sets.py
+
+def setify(a):
+    result = []
+
+    for item in a:
+        if item not in result:
+            result.append(item)
+
+    return result
+
+
+def diff(a, b):
+    result = []
+
+    for item in a:
+        if item not in b:
+            result.append(item)
+
+    return setify(result)
+
+
+def union(a, b):
+    return setify(a + b)
+
+
+def intersection(a, b):
+    result = []
+
+    for item in a:
+        if item in b:
+            result.append(item)
+
+    return setify(result)
+
+
+def cartesian_product(a, b):
+    # Tuple is (a, b) as a syntax
+    result = []
+
+    for x in a:
+        for y in b:
+            result.append((x, y))
+
+    return setify(result)
+
+
+def main():
+    print(setify([0, 1, 1, 5, 5, 6, 0, 1]) == [0, 1, 5, 6])
+    print(union([0, 0, 0, 0, 0], [1, 2]) == [0, 1, 2])
+    print(intersection([0, 0, 1, 2, 5], [5, 5, 6]) == [5])
+    print(diff([0, 1, 2, 3, 4, 5], [0, 0, 1, 1, 2, 2, 3, 3]) == [4, 5])
+    print(cartesian_product([0, 1, 1], [1, 0]))
+    print(cartesian_product([0, 1], [1]) == [(0, 1), (1, 1)])
+
+
+if __name__ == '__main__':
+    main()
---
+++ Week8/sort.py
@@ -0,0 +1,91 @@
+#Week8 Task1.1 sort.py
+
+def min_index(arr):
+    result = 0
+    index = 0
+
+    for x in arr:
+        if x < arr[result]:
+            result = index
+        index += 1
+
+    return result
+
+
+def selection_sort_that_destroys(numbers):
+    result = []
+    n = len(numbers)
+
+    while len(result) != n:
+        current_min_index = min_index(numbers)
+        result.append(numbers[current_min_index])
+        del numbers[current_min_index]
+
+    return result
+
+
+def swap(i, j, items):
+    temp = items[i]
+    items[i] = items[j]
+    items[j] = temp
+
+
+def min_index_from(start_index, items):
+    result = start_index
+    n = len(items)
+
+    for index in range(start_index, n):
+        if items[index] < items[result]:
+            result = index
+        index += 1
+
+    return result
+
+
+def selection_sort_that_mutates(numbers):
+    n = len(numbers)
+
+    for i in range(0, n):
+        index_of_min_from_i = min_index_from(i, numbers)
+        swap(i, index_of_min_from_i, numbers)
+
+
+def diff(a, b):
+    result = []
+
+    for item in a:
+        if item not in b:
+            result.append(item)
+
+    return result
+
+
+def min_index_without_used(numbers, used):
+    unused_indexes = diff(range(0, len(numbers)), used)
+
+    min_index = unused_indexes[0]
+
+    for index in unused_indexes:
+        if numbers[index] < numbers[min_index]:
+            min_index = index
+
+    return min_index
+
+
+def selection_sort(numbers):
+    result = []
+    n = len(numbers)
+    used = []
+
+    while len(result) != n:
+        min_index = min_index_without_used(numbers, used)
+        used.append(min_index)
+        result.append(numbers[min_index])
+
+    return result
+
+
+a = [100, 0, 0, -10, 5]
+b = selection_sort(a)
+print(a)
+print(b)
